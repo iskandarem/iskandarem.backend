99e3c97c121dd054f78ec0a5f36c571ca7e4667c
iskandar-from-ubuntu-vm <iskandar@from-ubuntu.com>
2025-07-05 14:42:06 +0200

    created_at field removed from LikeSerializer; post and user marked as read_only in serializers because the will be set automaticly in backend using post_id from url and token to get user_id; same done for commentSerializer;

--- src/posts/serializers.py
+++ src/posts/serializers.py
@@ -9,9 +9,13 @@ class PostSerializer(ModelSerializer):
 class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
-        exclude = ['created_at', 'author', 'post']  # Exclude created_at, author, and post fields from the Comment model
+        exclude = ['created_at']  # Exclude created_at, author, and post fields from the Comment model
+        extra_kwargs = {'post': {'read_only': True}, 
+                        'author': {'read_only': True}}
 
 class LikeSerializer(ModelSerializer):
     class Meta:
         model = Like
-        exclude = ['created_at', 'post']  # Exclude created_at and post fields from the Like model
+        fields = ['post', 'user']
+        extra_kwargs = {'post': {'read_only': True}, 
+                        'user': {'read_only': True}}
